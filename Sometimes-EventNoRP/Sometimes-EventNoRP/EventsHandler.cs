dae0d8f1ec28760e2e3fd7c0740c22a99f3d7ca2
Kiore <mathias.kiore@gmail.com>
2026-02-14 17:45:44 +0100

    Add files via upload

---
+++ Sometimes-EventNoRP/Sometimes-EventNoRP/EventsHandler.cs
@@ -0,0 +1,38 @@
+﻿using LabApi.Events.Arguments.ServerEvents;
+using LabApi.Events.CustomHandlers;
+using LabApi.Features.Wrappers;
+
+namespace Sometimes_EventNoRP
+{
+    public class EventsHandler : CustomEventsHandler
+    {
+        private readonly Config _config;
+        public EventsHandler(Config config)
+        {
+            _config = config;
+        }
+        
+        protected void SubscribeEvents()
+        {
+            LabApi.Events.Handlers.ServerEvents.RoundStarting += OnServerRoundStarted;
+        }
+
+        protected void UnsubscribeEvents()
+        {
+            LabApi.Events.Handlers.ServerEvents.RoundStarting -= OnServerRoundStarted;
+        }
+        public void OnServerRoundStarted(RoundStartingEventArgs ev)
+        {
+            
+            System.Random random = new System.Random();
+            int roundStatus = random.Next(0, _config.NoRPPercentage);
+            
+            string message = roundStatus == 0 ? _config.MessageNoRP : _config.MessageRP;
+
+            foreach (var player in Player.List)
+            {
+                player.SendBroadcast(message, _config.BroadcastDuration);
+            }
+        }
+    }
+}
---
+++ Sometimes-EventNoRP/Sometimes-EventNoRP/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LabApi.Features;
+using LabApi.Loader.Features.Plugins;
+using LabApi.Loader.Features.Plugins.Enums;
+
+namespace Sometimes_EventNoRP
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name { get; } = "Sometimes-EventNoRP";
+        public override string Author { get; } = "Kiore";
+        public override string Description { get; } = "A plugin that launches a NoRP game at a random time of day.";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredApiVersion { get; } = new Version(LabApiProperties.CompiledVersion);
+        public override LoadPriority Priority { get; } = LoadPriority.Medium;
+
+        public override void Enable()
+        {
+        }
+        public override void Disable()
+        {
+        }
+    }
+}
